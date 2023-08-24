cd4ca70057a5318c546044554646f40cc353b7b1
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-08-24 20:55:06 +0600

    Create Cart Model

---
+++ lib/models/cart_model.dart
@@ -0,0 +1,11 @@
+class Cart {
+
+  late final int? id;
+  final String? productId;
+  final String? productName;
+  final int? initialPrice;
+  final int? productPrice;
+  final int? quantity;
+  final String? unitTag;
+  final String? image;
+}
