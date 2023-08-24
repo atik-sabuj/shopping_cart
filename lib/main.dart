70b5dc1e8a534850fec7209306701775e443344e
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-08-24 19:15:36 +0600

    Pkg Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_cart/pages/product_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
       ),
-      home: const HomePage(),
+      home: const ProductListScreen(),
     );
   }
 }
