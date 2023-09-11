327c807477a9f65b273dda670d1f1444a121d4f1
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-09-11 19:42:58 +0600

    Provider with Shared Preferences

--- lib/provider/cart_provider.dart
+++ lib/provider/cart_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CartProvider with ChangeNotifier{
 
@@ -8,5 +9,18 @@ class CartProvider with ChangeNotifier{
   double _totalPrice = 0.0;
   double get totalPrice => _totalPrice;
 
+  void _setPrefItems() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt('cart_item', _counter);
+    prefs.setDouble('totalPrice', _totalPrice);
+    notifyListeners();
+  }
+
+  void _getPrefItems() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _counter = prefs.getInt('cart_item') ?? 0;
+    _totalPrice = prefs.getDouble('totalPrice') ?? 0.0;
+    notifyListeners();
+  }
 
 }
