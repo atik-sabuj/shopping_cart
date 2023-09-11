eb00e1128ee130158884c8a76d236f77432471c1
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-09-11 19:03:15 +0600

    Db connect #2

--- lib/db/db_helper.dart
+++ lib/db/db_helper.dart
@@ -5,5 +5,18 @@ import 'package:shopping_cart/models/cart_model.dart';
 
 class DBHelper {
 
+  static Database? _db;
+
+  Future<Database?> get db async {
+    if(_db != null){
+      return _db!;
+    }
+
+    _db = await initDatabase();
+  }
+
+  initDatabase() async{
+    io.Directory documentDirectory = await getApplicationDocumentsDirectory();
+  }
 
 }
