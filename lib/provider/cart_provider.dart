b898e4243797f62449723d13dd2658e62815b84e
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-09-11 19:35:49 +0600

    Provider

---
+++ lib/provider/cart_provider.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CartProvider with ChangeNotifier{
+
+  int _counter = 0;
+  int get counter => _counter;
+
+  double _totalPrice = 0.0;
+  double get totalPrice => _totalPrice;
+
+
+}
