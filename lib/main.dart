c52b34dc907a15d8a9939daebcd0676064f942cd
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-09-11 20:00:57 +0600

    Provider with Shared Preferences #3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shopping_cart/pages/product_list.dart';
+import 'package:shopping_cart/provider/cart_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +13,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const ProductListScreen(),
+    return ChangeNotifierProvider(
+        create: (_) => CartProvider(),
+      child: Builder(builder: (BuildContext context){
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+          ),
+          debugShowCheckedModeBanner: false,
+          home: const ProductListScreen(),
+        );
+      },),
     );
   }
 }
