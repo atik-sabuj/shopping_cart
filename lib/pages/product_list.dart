9058ed0acda7d70326d5dc69bf74d47830c03139
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-09-12 18:11:02 +0600

    Cart Screen #5

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shopping_cart/db/db_helper.dart';
 import 'package:shopping_cart/models/cart_model.dart';
+import 'package:shopping_cart/pages/cart_screen.dart';
 import 'package:shopping_cart/provider/cart_provider.dart';
 import 'package:badges/badges.dart' as badges;
 
@@ -40,17 +41,25 @@ class _ProductListScreenState extends State<ProductListScreen> {
         title: Text('Product List'),
         centerTitle: true,
         actions: [
-          Center(
-            child: badges.Badge(
-              badgeContent: Consumer<CartProvider>(
-                builder: (context, value, child){
-                  return Text(value.getCounter().toString(),
-                      style: TextStyle(color: Colors.white));
-                },
+          InkWell(
+            onTap: (){
+              Navigator.push(
+                  context, MaterialPageRoute(
+                  builder: (context) => CartScreen()));
+            },
 
+            child: Center(
+              child: badges.Badge(
+                badgeContent: Consumer<CartProvider>(
+                  builder: (context, value, child){
+                    return Text(value.getCounter().toString(),
+                        style: TextStyle(color: Colors.white));
+                  },
+
+                ),
+               //animationDuration: Duration(milliseconds: 300),
+                child: Icon(Icons.shopping_bag_outlined),
               ),
-             //animationDuration: Duration(milliseconds: 300),
-              child: Icon(Icons.shopping_bag_outlined),
             ),
           ),
 
