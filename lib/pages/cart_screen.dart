5f31035dcb7f128918bf6d2f7a7ed84284f4f62c
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-09-13 20:51:43 +0600

    Add Empty cart

--- lib/pages/cart_screen.dart
+++ lib/pages/cart_screen.dart
@@ -277,3 +277,25 @@ class ReusableWidget extends StatelessWidget {
     );
   }
 }
+
+
+
+class ReusableWidget extends StatelessWidget {
+  final String title, value;
+  const ReusableWidget({required this.title, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(title, style: Theme.of(context).textTheme.subtitle2,),
+          Text(value.toString(), style: Theme.of(context).textTheme.subtitle2,)
+        ],
+      ),
+    );
+  }
+}
+
